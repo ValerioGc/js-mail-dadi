a754c24ea071c96b7f379d46b614977960f84d15
ValerioGc <valerio.gc1@gmail.com>
2022-07-07 15:49:57 +0200

    Revisione finale

--- Gioco dei dadi/js/main.js
+++ Gioco dei dadi/js/main.js
@@ -6,19 +6,30 @@ btn.addEventListener ('click',
         let pc_try = Math.ceil(Math.random() * 5) + 1;
         let user_try = Math.ceil(Math.random() * 5) + 1;
         user_points.innerHTML = `Il punteggio dell'utente é  ${user_try} `;
-        pc_points.innerHTML = `Il punteggio dell'utente é  ${pc_try} `;
+        user_points.classList = 'pad-y';
+        pc_points.innerHTML = `Il punteggio del PC é  ${pc_try} `;
+        pc_points.classList = 'pad-y';
         let result;
         if (pc_try > user_try) {
-            result = "Hai perso!"
+            result = "Hai Perso!";
         } else if (pc_try < user_try) {
-            result = "Hai Vinto!"
+            result = "Hai Vinto!";
         } else {
-            result = "Pareggio!"
+            result = "Pareggio!";
         }
         let risultato = document.getElementById('risultato');
         risultato.innerHTML = result;
-        risultato.style.backgroundColor = "LightGrey";
-        risultato.style.fontSize = "20px";
-        risultato.style.fontWeight = "bold";
-        console.log(result)
+        risultato.style.display = 'block';
+        risultato.className = 'bdr';
+        if (result == 'Hai Vinto!') {
+            risultato.style.backgroundColor = "LightGreen";
+            risultato.style.Color = "white";
+        }
+        else if (result == 'Hai Perso!') {
+            risultato.style.backgroundColor = "Red";
+        }
+        else {
+            risultato.style.backgroundColor = "LightGrey";
+        }
+        console.log(result);
     });
